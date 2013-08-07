3028ef374047720626b623b0740061d8053cd3b3
Nicholas Clarke <nc6@sanger.ac.uk>
2013-08-07 11:19:58 +0100

    Make system calls 'safe'.
    All calls out to system are now done in a clean environment, to avoid
    against an external attacker modifying environment variables in order
    to execute other programs as root.
    
    Also, where possible external calls have been replaced with library
    functions using FFI.

--- Hgc/Cvmfs.hs
+++ Hgc/Cvmfs.hs
@@ -2,11 +2,12 @@
 Handles CVMFS commands.
 -}
 module Hgc.Cvmfs where
-  import System.Cmd (rawSystem)
   import System.Exit (ExitCode(..))
   import Control.Exception (catch)
   import Control.Monad (when)
   import System.Log.Logger
+
+  import Hgc.Shell (safeSystem)
 
   -- | CVMFS Base directory
   base :: FilePath
@@ -15,20 +16,20 @@ module Hgc.Cvmfs where
   -- | Start a CVMFS transaction
   transaction :: String -- ^ repository name
               -> IO ()
-  transaction repo = rawSystem "cvmfs_server" ["transaction", repo] >>= \ex -> case ex of
+  transaction repo = safeSystem "cvmfs_server" ["transaction", repo] >>= \ex -> case ex of
     ExitSuccess -> return ()
     ExitFailure r -> ioError . userError $ "Cannot start transaction (exit code " ++ show r ++ ")."
 
   -- | Publish a CVMFS repository
   publish :: String -> IO ()
-  publish repo = rawSystem "cvmfs_server" ["publish", repo] >>= \ex -> case ex of
+  publish repo = safeSystem "cvmfs_server" ["publish", repo] >>= \ex -> case ex of
     ExitSuccess -> return ()
     ExitFailure r -> ioError . userError $ "Cannot publish repo (exit code " ++ show r ++ ")."
 
   -- | Abort a transaction on a CVMFS repository.
   abort :: String -- ^ repository name
         -> IO ()
-  abort repo = rawSystem "cvmfs_server" ["abort", "-f", repo] >>= \ex -> case ex of
+  abort repo = safeSystem "cvmfs_server" ["abort", "-f", repo] >>= \ex -> case ex of
     ExitSuccess -> return ()
     ExitFailure r -> ioError . userError $ "Cannot abort transaction (exit code " ++ show r ++ ")."
 
--- Hgc/Shell.hs
+++ Hgc/Shell.hs
@@ -2,16 +2,37 @@
 Handles miscellaneous other shell commands.
 -}
 
-module Hgc.Shell where 
+module Hgc.Shell (
+      cp
+    , touch
+    , mkdir
+    , safeSystem
+  ) where 
   import System.Cmd
   import System.Exit
   import System.Directory
+  import System.Posix.Files
+  import System.Process
 
   cp :: FilePath -> FilePath -> IO ExitCode
   cp src dest = rawSystem "cp" ["-r", src, dest]
 
-  touch :: FilePath -> IO ExitCode
-  touch fp = rawSystem "touch" [fp]
+  touch :: FilePath -> IO ()
+  touch = touchFile 
 
   mkdir :: FilePath -> IO ()
   mkdir = createDirectoryIfMissing True
+
+  -- | Execute an external process in a 'safe' way.
+  safeSystem :: String -> [String] -> IO ExitCode
+  safeSystem command args = 
+    createProcess p >>= \(_,_,_,ph) ->
+    waitForProcess ph
+    where
+      p = (proc command args) { env = safeEnv }
+      safeEnv = Just [
+          ("SHELL", "/bin/bash")
+        , ("PATH", "/usr/local/bin:/usr/bin:/bin")
+        , ("LANG", "en_GB.UTF-8")
+        , ("IFS", "")
+        ]
