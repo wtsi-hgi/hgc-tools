5342cc581065a9fb797d6d6d350bd899deae2d7c
Nicholas Clarke <nc6@sanger.ac.uk>
2013-08-19 18:48:35 +0100

    First drop of stuff to clean temporary files.

---
+++ Hgc/Directory.hs
@@ -0,0 +1,52 @@
+module Hgc.Directory where
+  import Control.Monad (when)
+  import System.Directory (
+      getDirectoryContents
+    , removeDirectory
+    , removeFile
+    )
+  import System.FilePath ((</>))
+  import System.Posix.Files
+  import System.Posix.Types (UserID, GroupID)
+
+  data FollowSymlinks = Follow | NoFollow
+
+  removeDirectoryRecursive :: FollowSymlinks
+                           -> FilePath
+                           -> IO ()
+  removeDirectoryRecursive fs fp = do
+    cont <- getDirectoryContents fp
+    sequence_ [rm (fp </> x) | x <- cont, x /= "." && x /= ".."]
+    removeDirectory fp
+    where
+      rm :: FilePath -> IO ()
+      rm f = do
+        stat <- getStatus f
+        case (isDirectory stat, isSymbolicLink stat) of
+          (True, _) -> removeDirectoryRecursive fs f
+          (_, True) -> removeLink f
+          _ -> removeFile f
+      getStatus = case fs of
+        Follow -> getFileStatus
+        NoFollow -> getSymbolicLinkStatus
+
+  chownRecursive :: FollowSymlinks
+                 -> UserID
+                 -> GroupID -- if set to -1, do not change
+                 -> FilePath
+                 -> IO ()
+  chownRecursive fs uid gid fp =
+    chownRecursive' fp where 
+      chownRecursive' f = do
+        cont <- getDirectoryContents f
+        sequence_ [chown (f </> x) | x <- cont, x /= "." && x /= ".."]
+        setOwner f
+      chown f = do
+        stat <- getStatus f
+        when (isDirectory stat) $ chownRecursive' f
+      getStatus = case fs of
+        Follow -> getFileStatus
+        NoFollow -> getSymbolicLinkStatus
+      setOwner f = case fs of
+        Follow -> setOwnerAndGroup f uid gid
+        NoFollow -> setSymbolicLinkOwnerAndGroup f uid gid
